5e23208e44da64126827b546782a29170ce10209
futureapocalypse <future-apocalypsee@protonmail.com>
2025-07-20 17:56:00 +0300

    Initial Commit

---
+++ AuraCast/AuraCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraCastApp.swift
+//  AuraCast
+//
+//  Created by Mihail Verejan on 20.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AuraCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
